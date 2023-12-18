85e547b85a6d602badb0f4753664665044f02481
DaviDisDev <david.pr.dev@gmail.com>
2023-12-18 15:59:39 +0100

    exercice3.12

---
+++ part3/mongoDBPhonebook3.12/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const content = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://fullstack_helsinki:${password}@cluster0.ie6uhog.mongodb.net/phonebook-app?retryWrites=true`;
+mongoose.connect(url)
+
+const contactSchema = new mongoose.Schema({
+  content: String,
+  number: String
+})
+
+const Contact = mongoose.model('contact', contactSchema)
+
+if(content && number ){
+
+  const person = new Contact({
+    content: content,
+    number: number
+  });
+
+  person.save().then(result => {
+    console.log('Added',result.content, result.number)
+    mongoose.connection.close()
+  })
+}else{
+
+  Contact.find({}).then(result => {
+    result.forEach(contact => {
+      console.log(contact)
+    })
+    mongoose.connection.close()
+  })
+}
